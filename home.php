90023f2494cbdb6d89e1b29bbc4ad8688a7601d5
Enis <xmarks.dev@gmail.com>
2025-11-07 15:21:47 +0100

    filter styles - desktop

--- home.php
+++ home.php
@@ -6,31 +6,28 @@
  * @package blast-2025
  */
 
-wp_enqueue_style( 'blast-archive' );
+wp_enqueue_style('blast-archive');
 
 get_header();
 
 // Get the page set as "Posts Page" for title/content
-$page_for_posts = get_option( 'page_for_posts' );
+$page_for_posts = get_option('page_for_posts');
 
-if ( $page_for_posts ):
-    $page = get_post( $page_for_posts );
+if ($page_for_posts):
+    $page = get_post($page_for_posts);
 
     // Check if title should be hidden
-    $hide_title = get_field( 'hide_title', $page_for_posts );
+    $hide_title = get_field('hide_title', $page_for_posts);
 
-    if ( ! $hide_title ): ?>
-        <h1 class="page-title"><?= esc_html( $page->post_title ); ?></h1>
+    if (!$hide_title): ?>
+        <h1 class="page-title"><?= esc_html($page->post_title); ?></h1>
     <?php endif;
 
     // Optionally display page content
     // echo apply_filters('the_content', $page->post_content);
-endif; ?>
-    <div class="container container--blog-filter">
-        <?php
-        // Render blog filter shortcode ONCE
-        echo do_shortcode( '[blast-blog-filter]' );
-        ?>
-    </div>
-<?php
+endif;
+
+// Render blog filter shortcode ONCE
+echo do_shortcode('[blast-blog-filter]');
+
 get_footer();
--- inc/helpers.php
+++ inc/helpers.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 
 function cc_mime_types($mimes) {
 	$mimes['json'] = 'application/json';
-	$mimes['svg'] = 'image/svg+xml'; 
+	$mimes['svg'] = 'image/svg+xml';
 	return $mimes;
 }
 
@@ -19,7 +19,7 @@ function sprite_svg( $spriteName, $svgWidth = '24', $svgHeight = '24', $return =
 	if (isset($svgHeight)) {
 		$elHeight = 'height="' . $svgHeight . '"';
 	}
-	$iconHtml = '<svg class="svg-icon '. $spriteName .'" '.$elWidth.' '.$elHeight.'"><use xlink:href="' . $svg . '"></use></svg>';
+	$iconHtml = '<svg class="svg-icon '. $spriteName .'" '.$elWidth.' '.$elHeight.'><use xlink:href="' . $svg . '"></use></svg>';
 	if ($return) {
 		return $iconHtml;
 	} else {
@@ -136,30 +136,30 @@ function add_arrows_to_gutenberg_buttons($content) {
     if (is_admin() || empty(trim($content))) {
         return $content;
     }
-    
+
     // Check if content contains button blocks
     if (strpos($content, 'wp-block-button') === false) {
         return $content;
     }
-    
+
     // Use DOMDocument for reliable HTML parsing
     libxml_use_internal_errors(true);
     $dom = new DOMDocument();
     $dom->loadHTML('<?xml encoding="utf-8" ?>' . $content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-    
+
     // Find all button links
     $xpath = new DOMXPath($dom);
     $buttonLinks = $xpath->query('//div[contains(@class, "wp-block-button")]//a[contains(@class, "wp-block-button__link")]');
-    
+
     foreach ($buttonLinks as $buttonLink) {
         // Add arrow icon class
         $currentClass = $buttonLink->getAttribute('class');
         $buttonLink->setAttribute('class', $currentClass . ' has-arrow-icon');
-        
+
         // Get current button text
         $buttonText = $buttonLink->textContent;
-		
-        
+
+
         // Create simple arrow SVG (inline)
         $arrowSvg = '<svg class="svg-icon icon-arrow-right" width="14" height="10" viewBox="0 0 14 10" fill="currentColor">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0441 4.59009H0.75H12.0441Z"/>
@@ -170,10 +170,10 @@ function add_arrows_to_gutenberg_buttons($content) {
             <path d="M9.21875 1.06067L12.7482 4.59008" stroke="currentColor" stroke-width="1.5" stroke-linecap="square"/>
         </svg>';
 
-        
+
         // Clear the button content and add text + arrow
         $buttonLink->nodeValue = '';
-        
+
         // Create text wrapper span
         $textWrapper = $dom->createElement('span');
         $textWrapper->setAttribute('class', 'button-text');
@@ -181,27 +181,27 @@ function add_arrows_to_gutenberg_buttons($content) {
         $textNode = $dom->createTextNode($buttonText);
         $textWrapper->appendChild($textNode);
         $buttonLink->appendChild($textWrapper);
-        
+
         // Create arrow wrapper
         $arrowWrapper = $dom->createElement('span');
         $arrowWrapper->setAttribute('class', 'button-arrow-wrapper');
-        
+
         // Add the SVG to arrow wrapper
         $arrowFragment = $dom->createDocumentFragment();
         $arrowFragment->appendXML($arrowSvg);
         $arrowWrapper->appendChild($arrowFragment);
-        
+
         $buttonLink->appendChild($arrowWrapper);
     }
-    
+
     // Get the modified HTML
     $modifiedContent = $dom->saveHTML();
-    
+
     // Remove the XML declaration that DOMDocument adds
     $modifiedContent = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace(['<?xml encoding="utf-8" ?>', '<html><body>', '</body></html>'], '', $modifiedContent));
-    
+
     libxml_clear_errors();
-    
+
     return $modifiedContent;
 }
 add_filter('the_content', 'add_arrows_to_gutenberg_buttons', 20);
